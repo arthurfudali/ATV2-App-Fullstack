95ea14dfbdb526ace38495d78fd282a2df144ac9
Arthur Fudali <arthurfudali5@gmail.com>
2026-05-30 16:01:45 -0300

    Flyway connection

--- server/src/main/kotlin/com/fatec/at2_base/db/migrations/FlywayConnection.kt
+++ server/src/main/kotlin/com/fatec/at2_base/db/migrations/FlywayConnection.kt
@@ -1,4 +1,10 @@
 package com.fatec.at2_base.db.migrations
 
-class FlywayConnection {
+import java.sql.Connection
+
+class FlywayConnection(private val delegate: Connection) : Connection by delegate {
+    override fun close() {}
+    override fun setTransactionIsolation(level: Int) {}
+    override fun setAutoCommit(autoCommit: Boolean) {}
+    override fun setReadOnly(readOnly: Boolean) {}
 }
